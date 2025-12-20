f0b540393912cdab6bef0fb326c2c4c5df155de3
Shoumyo <shoumyosrijito@gmail.com>
2025-12-20 23:02:52 +0600

    constructor of class and it's type concept added by code

--- OOP/Syntax/syntax.cpp
+++ OOP/Syntax/syntax.cpp
@@ -61,9 +61,51 @@ class Student{
         this->roll=roll;
         this->age=age;
     }
+
+    //copy constructor
+    Student(Student &orgObj){  //pass by reference
+        cout<<"Custom copy constructor called"<<endl;
+        this->name=orgObj.name;
+        this->roll=orgObj.roll;
+        this->age=orgObj.age;
+    }
+
+    void display(){
+        cout<<name<<endl;
+        cout<<roll<<endl;
+        cout<<age<<endl;
+    }
+};
+
+class Boys{
+    public:
+    string name;
+    double* cgpaptr;
+    Boys(string name,double cgpa){
+        this->name=name;
+        cgpaptr=new double;  //dynamically memory allocation
+        *cgpaptr=cgpa;
+    }
+
+    Boys(Boys &obj){
+        this->name=obj.name;
+        this->cgpaptr=obj.cgpaptr;
+    }
+
+    void getInfo(){
+        cout<<name<<endl;
+        cout<<*cgpaptr<<endl;
+    }
 };
 
 int main(){
+
+    Boys b1("shoumyo",3.3);
+    Boys b2(b1);   // shallow copy
+    b2.getInfo();
+    *(b2.cgpaptr)=9.2;  //can be changed by dynamic memory allocation
+    // deep copy
+    b2.getInfo();
 
     //object
     Teacher t1; //constructor call automatically
@@ -73,8 +115,14 @@ int main(){
     t1.dept="CSE";
     // t1.salary=25000;  not accessible cuz it is private
     t1.setSalary(25000);
+    Teacher t2(t1);  // default copy constructor invoke
 
     t1.display();
+    t2.display();
+
+    Student s1("shoumyo",2304132,23);
+    Student s2(s1);   //custom copy constructor called
+    s2.display();
 
     return 0;
 }
