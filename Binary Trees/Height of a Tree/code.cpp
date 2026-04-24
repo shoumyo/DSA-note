10dcbe3ec4996934411c0c993fb26da8719c4965
Shoumyo <shoumyosrijito@gmail.com>
2026-04-24 20:23:07 +0600

    Height of a tree

---
+++ Binary Trees/Height of a Tree/code.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+static int idx=-1;
+Node* BuildTree(vector<int>preorder){
+    idx++;
+    if(preorder[idx]==-1){
+        return NULL;
+    }
+    Node* root=new Node(preorder[idx]);
+    root->left=BuildTree(preorder);
+    root->right=BuildTree(preorder);
+
+    return root;
+}
+
+int heightofTree(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int leftH=heightofTree(root->left);
+    int rightH=heightofTree(root->right);
+    return max(leftH,rightH)+1;
+}
+
+int main(){
+
+    vector<int>preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root=BuildTree(preorder);
+    cout<<heightofTree(root)<<endl;
+
+    return 0;
+}
