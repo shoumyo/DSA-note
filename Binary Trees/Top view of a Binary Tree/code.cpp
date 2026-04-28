a8104117563ec6f2e83c08679919377b80999734
Shoumyo <shoumyosrijito@gmail.com>
2026-04-28 08:23:56 +0600

    Top view of a Binary Tree

---
+++ Binary Trees/Top view of a Binary Tree/code.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+static int idx=-1;
+Node* BuildTree(vector<int>preorder){
+    idx++;
+    if(preorder[idx]==-1){
+        return NULL;
+    }
+    Node* root=new Node(preorder[idx]);
+    root->left=BuildTree(preorder);
+    root->right=BuildTree(preorder);
+
+    return root;
+}
+
+void Topview(Node* root){
+    queue<pair<Node*,int>>q;
+    map<int,int>m;
+    q.push({root,0});
+    while(q.size()>0){
+        Node* curr=q.front().first;
+        int currHD=q.front().second;
+        q.pop();
+        if(m.find(currHD)==m.end()){
+            m[currHD]=curr->data;
+        }
+        if(curr->left!=NULL){
+            q.push({curr->left,currHD-1});
+        }
+        if(curr->right!=NULL){
+            q.push({curr->right,currHD+1});
+        }
+    }
+    for(auto val:m){
+        cout<<val.second<<" ";
+    }
+    cout<<endl;
+
+}
+
+int main(){
+
+    vector<int>preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root=BuildTree(preorder);
+    Topview(root);
+    return 0;
+}
